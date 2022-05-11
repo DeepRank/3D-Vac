57453853d042ab8578b7ebf0e56dd7d7358ea806
gcroci2 <crocioni.giulia@gmail.com>
2022-05-11 16:19:55 +0200

    Initialized the repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Personalized cancer vaccine design through 3D modelling boosted geometric learning',
+    author='DeepRank',
+    license='MIT',
+)
