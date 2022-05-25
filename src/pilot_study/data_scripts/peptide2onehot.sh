88951f500ac4b1c25ee6107f4265d56b30810f48
DanLep97 <danycmoilol@gmail.com>
2022-05-25 18:29:34 +0200

    deleted data_scripts

--- src/pilot_study/data_scripts/peptide2onehot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -p thin
-#SBATCH -N 1
-#SBATCH -n 128
-#SBATCH -c 1
-#SBATCH --time 01:00:00
-#SBATCH -o /projects/0/einf2380/data/modelling_logs/peptide2onehot.out
-
-mpiexec -n 128 python -u ./peptide2onehot.py
