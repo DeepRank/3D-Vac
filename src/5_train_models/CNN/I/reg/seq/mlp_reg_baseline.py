aa6cdfec4750475983ba5218525f64a87e146862
DanLep97 <danycmoilol@gmail.com>
2022-05-30 16:17:30 +0200

    last commit before switching to 0 branch

--- src/5_train_models/CNN/I/reg/seq/mlp_reg_baseline.py
+++ src/5_train_models/CNN/I/reg/seq/mlp_reg_baseline.py
@@ -2,16 +2,15 @@ from urllib.request import Request
 import torch
 import argparse
 from torch import nn
-from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
 from torch.utils.data import Subset
+from torch.utils.data import DataLoader
 import copy
-import blosum
 import os.path as path
 import os
 import sys
 sys.path.append(path.abspath("../../../../"))
 from CNN.models import MlpRegBaseline
+from CNN.datasets import Peptides
 import random
 # import multiprocessing as mp
 from mpi4py import MPI
@@ -77,8 +76,6 @@ a = arg_parser.parse_args()
 mpi_conn = MPI.COMM_WORLD
 rank = mpi_conn.Get_rank()
 size = mpi_conn.Get_size()
-print(f"number of threads used by torch on {rank} is {torch.get_num_threads()}")
-print(f"number of cpu used by torch on {rank} is {os.cpu_count()}")
 datasets = []
 
 best_model = {
@@ -90,7 +87,6 @@ best_model = {
 }
 
 # onehot (sparse) encoding
-aminoacids = ('ACDEFGHIKLMNPQRSTVWY')
 
 # FUNCTIONS AND USEFUL STUFF
 #----------------------------
@@ -99,6 +95,11 @@ def normalize(ds,training_mean,training_std):
     ds = torch.log(ds)
     ds = (ds-training_mean)/training_std
     return torch.sigmoid(ds)
+
+def renorm(ds, training_mean, training_std):
+    ds = torch.logit(ds)
+    ds = ds*training_std+training_mean
+    return torch.exp(ds)
 
 # define the train function
 def train_f(dataloader, model, loss_fn, optimizer,device,e):
@@ -140,63 +141,24 @@ epochs = a.epochs
 # if CUDA cores available, use them and not CPU
 device = ("cpu", "cuda")[torch.cuda.is_available()]
 
-# the whole dataset class, which extends the torch Dataset class
-class Peptides(Dataset):
-    def __init__(self):
-        self.ba_values = torch.tensor(csv_ba_values, dtype=torch.float64)
-        self.peptides = peptides
-    def __getitem__(self, idx):
-        return self.peptides[idx], self.ba_values[idx]
-    def __len__(self):
-        return len(self.peptides)
 
 # sparse encoding
-def peptide2onehot(peptide):
-    AA_eye = torch.eye(20, dtype=torch.float)
-    return [AA_eye[aminoacids.index(res)].tolist() for res in peptide]
 
 # blosum encoding 
-def peptide2blosum(peptide):
-    mat = blosum.BLOSUM(62)
-    blosum_t = [[]]
-    blosum_aa = ["A"]
-    for aa in mat.keys():
-        if aa[0] in aminoacids and aa[1] in aminoacids:
-            if len(blosum_t[-1]) < 20:
-                blosum_t[-1].append(mat[aa])
-            else:
-                blosum_aa.append(aa[0])
-                blosum_t.append([mat[aa]])
-    blosum_t = torch.tensor(blosum_t)
-    blosum_aa = "".join(blosum_aa)
-    return [blosum_t[blosum_aa.index(res)].tolist() for res in peptide]
 
 # DATA PREPROCESSING
 #----------------------------------------------
 if rank == 0:
-    print(f"Loading data and splitting it in unique datasets...")
-    ba_threshold = a.threshold
-    csv_path = a.csv_file
-    csv_peptides = []
-    csv_ba_values = []
-    with open(csv_path) as csv_f:
-        rows = [row.replace("\n", "").split(",") for row in csv_f]
-        for row in rows:
-            if float(row[3]) <= ba_threshold:
-                csv_peptides.append(row[2])
-                csv_ba_values.append(float(row[3]))
-
-    if a.encoder == "blosum":
-        peptides = torch.tensor([peptide2blosum(p) for p in csv_peptides])
-    else:
-        peptides = torch.tensor([peptide2onehot(p) for p in csv_peptides])
+    print("Loading data...")
+    dataset = Peptides(a.threshold, a.csv_file, a.encoder)
+    print("Data loaded, splitting into unique test datasets...")
 
     # SEPARATE TRAIN VALIDATION AND TEST DATASETS
     # -------------------------------------------
 
     # instantiate the Peptides class and split the dataset for 10 testing unique cases
-    peptides_to_list = peptides.tolist() # used to retrieve the indices for train and validation without the current test
-    test_peptides = peptides.tolist() #this will be substracted each iteration and newly created datasets will not have redundant test sets
+    peptides_to_list = dataset.peptides.tolist() # used to retrieve the indices for train and validation without the current test
+    test_peptides_indices = range(len(dataset.peptides.tolist())) #this will be substracted each iteration and newly created datasets will not have redundant test sets
     ds_l = len(peptides_to_list)
     num_test_sets = int(100*test_p)
 
@@ -206,12 +168,11 @@ if rank == 0:
     test_per_dataset = int(test_p*ds_l)
 
     for d in range(num_test_sets):
-        test_peptides_indices = range(len(test_peptides))
         test_indices = random.sample(test_peptides_indices, test_per_dataset)
 
         ds_indices = list(range(ds_l)) # create the indices range and remove the indices of test
         for i in sorted(test_indices, reverse = True):
-            del ds_indices[peptides_to_list.index(test_peptides[i])] # remove the test indice so it is not used for training and val
+            del ds_indices[peptides_to_list.index(test_peptides_indices[i])] # remove the test indice so it is not used for training and val
             del test_peptides[i] # remove it from the test peptide copy to avoid test redundancies in following datasets
 
         # create the train and validation indices without the test indices in it:
@@ -224,27 +185,32 @@ if rank == 0:
         if len([train for train in train_indices if train in validation_indices]) > 0:
             print("Something went wrong, there is redundancies in the train and validation dataset")
         else:
-            dataset = Peptides()
 
             training_mean = torch.log(dataset.ba_values[train_indices]).mean()
             training_std = torch.log(dataset.ba_values[train_indices]).std()
+            training_min = torch.log(dataset.ba_values[train_indices]).min()
+            training_max = torch.log(dataset.ba_values[train_indices]).max()
             
 
+            # dataset.ba_values = normalize(dataset.ba_values, training_mean, training_std)
             dataset.ba_values = normalize(dataset.ba_values, training_mean, training_std)
             train = Subset(dataset, train_indices)
             validation = Subset(dataset, validation_indices)
-            test = Subset(dataset, test_indices)
 
             # create the dataloaders iterators:
             train_dataloader = DataLoader(train, batch_size=batch)
             validation_dataloader = DataLoader(validation, batch_size=batch)
-            test_dataloader = DataLoader(test, batch_size = batch)
+
             datasets.append({
-                "train_dataloader": train_dataloader,
-                "validation_dataloader": validation_dataloader,
-                "test_subset": test,
-                "num": d
+                "train_dataloader": copy.deepcopy(train_dataloader),
+                "validation_dataloader": copy.deepcopy(validation_dataloader),
+                "test_indices": test_indices,
+                "renorm_values": {
+                    "training_max": training_max,
+                    "training_min": training_min
+                },
             })
+            dataset.ba_values = renorm(dataset.ba_values, training_mean, training_std)
     # CREATE MULTIPROCESSING
     #-----------------------
 
@@ -255,7 +221,6 @@ if rank==0:
 # that's the whole multiprocessing code for slaves
 train_dataloader = split["train_dataloader"]
 validation_dataloader = split["validation_dataloader"]
-test_subset = split["test_subset"]
 # TRAIN THE MODEL
 #----------------
 
@@ -293,12 +258,7 @@ print(f"Training on {rank} finished.")
 best_model["train_losses"] = train_losses
 best_model["validation_losses"] = validation_losses
 best_model["model"] = best_model["model"].state_dict()
-test_X = test_subset.dataset.peptides[test_subset.indices]
-test_y = test_subset.dataset.ba_values[test_subset.indices]
-best_model["test_data"] = {
-    "test_X": test_X,
-    "test_y": test_y
-}
+best_model["test_indices"] = split["test_indices"]
 
 # GATHER THE DATA
 #--------------
