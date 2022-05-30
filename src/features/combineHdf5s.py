7898d59e7819fc561dc8d736761b25bfe3d29169
DTRademaker <dtrademaker@gmail.com>
2022-05-30 12:09:03 +0200

    updated features script and added a python script that can combine hdf5s

---
+++ src/features/combineHdf5s.py
@@ -0,0 +1,35 @@
+import glob
+import h5py
+import os 
+
+# Recursive function
+def add_hdf5(input_hdf5, key, output_hdf5):
+	if key in output_hdf5:
+		return
+	if type(input_hdf5[key]) == h5py.Group:
+		out_group = output_hdf5.require_group(key)
+		for child_key in input_hdf5[key]:
+			add_hdf5(input_hdf5[key], child_key, out_group)
+		for key, value in input_hdf5[key].attrs.items():
+			out_group.attrs[key] = value
+	elif type(input_hdf5[key]) == h5py.Dataset:
+		output_hdf5.create_dataset(key, data=input_hdf5[key][()])
+	else:
+		raise TypeError(type(input_hdf5[key]))
+
+# combines all hdf5 from a folder into a single hdf5 file
+def folder2hdf5(folder_location, output_file_name='combined.hdf5'):
+	# find all hdf5 files in folder
+	hdf5_files = glob.glob('%s/*.hdf5' % folder_location)
+	# Create an output hdf5 file
+	output_hdf5 = h5py.File('%s/%s' % (folder_location, output_file_name), 'w')
+	# loop over all folders
+	for input_hdf5path in hdf5_files:
+		input_hdf5 = h5py.File(input_hdf5path, 'r')
+		for entry_id in input_hdf5.keys():
+			add_hdf5(input_hdf5, entry_id, output_hdf5)
+		os.remove(input_hdf5path)
+
+if __name__ == "__main__":
+	folder2hdf5(folder_location='hdf5output', output_file_name='combined.hdf5')
+
