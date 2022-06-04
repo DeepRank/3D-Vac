4eaf2152c2a7a64e47ab2ba04c646beeb51f7062
DanLep97 <danycmoilol@gmail.com>
2022-06-04 14:19:02 +0200

    updated function to load the csv which outputs the group labels if needed

--- src/5_train_models/CNN/datasets.py
+++ src/5_train_models/CNN/datasets.py
@@ -2,6 +2,7 @@ from torch.utils.data import Dataset
 import torch
 import blosum
 import pickle
+import pandas as pd
 
 # encoding functions:
 aminoacids = ('ACDEFGHIKLMNPQRSTVWY')
@@ -39,25 +40,6 @@ class Reg_Seq_Dataset(Dataset):
     def __len__(self):
         return len(self.peptides)
 
-class Clust_Class_Seq_Dataset(Dataset):
-    def __init__(self, first_cluster, last_cluster, clusters, encoder):
-        self.peptides = []
-        self.labels = []
-        for i in range(first_cluster, last_cluster+1):
-            for peptide,label in clusters[str(i)]:
-                if encoder == "sparse": 
-                    peptide = peptide2onehot(peptide)
-                if encoder == "blosum":
-                    peptide = peptide2blosum(peptide)
-                self.peptides.extend(peptide)
-                self.labels.extend(label)
-        self.labels = torch.tensor(self.labels).long()
-        self.peptides = torch.stack(self.peptides)
-        def __getitem__(self,idx):
-            return self.peptides[idx], self.labels[idx]
-        def __len__(self,idx):
-            return len(self.peptides)
-
 class Class_Seq_Dataset(Dataset):
     def __init__(self, csv_peptides,labels, encoder):
         self.csv_peptides = csv_peptides
@@ -71,19 +53,6 @@ class Class_Seq_Dataset(Dataset):
     def __len__(self):
         return len(self.peptides)
 
-def load_clust_class_seq_data(cluster_file, csv_path):
-    csv_rows = [row.split(",") for row in open(csv_path)]
-    pkl_clusters = pickle.load(open(cluster_file))
-    clusters = {}
-    for c_idx, c_peptides in enumerate(pkl_clusters.values()):
-        cluster = []
-        for peptide in c_peptides:
-            ba_values = torch.tensor([float(row[3]) for row in csv_rows if row[2] == peptide])
-            label = (0.,1.)[ba_values.mean() <= 500]
-            cluster.append((peptide,label))
-        clusters[str(c_idx)] = cluster
-    return clusters
-
 def load_reg_seq_data(csv_file, threshold):
     csv_peptides = []
     csv_ba_values = []
@@ -95,14 +64,20 @@ def load_reg_seq_data(csv_file, threshold):
                 csv_ba_values.append(float(row[3]))
     return csv_peptides, csv_ba_values
 
-def load_class_seq_data(csv_file, threshold):
+def load_class_seq_data(csv_file, threshold, group=False): # if cluster_file is set, performs a clustered data loading
     csv_peptides = []
     labels = []
-    with open(csv_file) as csv_f:
-        rows = [row.replace("\n", "").split(",") for row in csv_f]
-        for row in rows:
-            csv_peptides.append(row[2])
-            labels.append((0.,1.)[float(row[3]) <= threshold])
+    groups = [] 
+    df = pd.read_csv(csv_file)
+
+    csv_peptides = df["peptide"].tolist()
+
+    # binder or non binder if the mean value of redundant peptides less than the threshold:
+    labels = [(0.,1.,)[ df[df["peptide"] == peptide]["measurement_value"].mean() < threshold ] for peptide in csv_peptides]
+
+    if group: # peptides grouped by clusters for the clustered classification
+        groups = df["cluster"].tolist() # used for LeaveOneGroupOut sklearn function when doing the clustered classification
+        return csv_peptides, labels, groups
     return csv_peptides, labels
 
 # functions to transform/transform back binding affinity values
