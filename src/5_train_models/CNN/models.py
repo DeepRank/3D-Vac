05ce0c54d4973b8b5b6616af2edddd64608f98ac
DanLep97 <danycmoilol@gmail.com>
2022-06-11 14:59:59 +0200

    trying without dropout

--- src/5_train_models/CNN/models.py
+++ src/5_train_models/CNN/models.py
@@ -7,17 +7,17 @@ class MlpRegBaseline(nn.Module):
 
             nn.Linear(input, neurons_per_layer),
             # nn.BatchNorm1d(neurons_per_layer),
-            nn.Dropout(),
+            # nn.Dropout(),
             nn.ReLU(),
 
             nn.Linear(neurons_per_layer, neurons_per_layer),
             # nn.BatchNorm1d(int(neurons_per_layer/2)),
-            nn.Dropout(),
+            # nn.Dropout(),
             nn.ReLU(),
 
             nn.Linear(neurons_per_layer, neurons_per_layer),
             # nn.BatchNorm1d(int(neurons_per_layer/4)),
-            nn.Dropout(),
+            # nn.Dropout(),
             nn.ReLU(),
 
             nn.Linear(neurons_per_layer, outputs),
