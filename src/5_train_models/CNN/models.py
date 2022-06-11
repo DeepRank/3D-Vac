02623afd7689579b092bbb53d5a9da48aab9c4f3
DanLep97 <danycmoilol@gmail.com>
2022-06-11 16:54:16 +0200

    added batchnorm

--- src/5_train_models/CNN/models.py
+++ src/5_train_models/CNN/models.py
@@ -3,24 +3,24 @@ class MlpRegBaseline(nn.Module):
     def __init__(self, input = 20*9, neurons_per_layer=1000, outputs=1 ):
         super(MlpRegBaseline,self).__init__()
         self.architecture = nn.Sequential(
+            # input layer
+            nn.BatchNorm1d(int(input/9)),
             nn.Flatten(),
-
+            
+            # hidden layer
             nn.Linear(input, neurons_per_layer),
-            # nn.BatchNorm1d(neurons_per_layer),
-            # nn.Dropout(),
+            nn.ReLU(),
+            nn.Dropout(), # half of weights are droped each forward()
+
+            nn.Linear(neurons_per_layer, neurons_per_layer),
             nn.ReLU(),
 
             nn.Linear(neurons_per_layer, neurons_per_layer),
-            # nn.BatchNorm1d(int(neurons_per_layer/2)),
-            # nn.Dropout(),
             nn.ReLU(),
 
-            nn.Linear(neurons_per_layer, neurons_per_layer),
-            # nn.BatchNorm1d(int(neurons_per_layer/4)),
-            # nn.Dropout(),
-            nn.ReLU(),
-
+            # output layer
             nn.Linear(neurons_per_layer, outputs),
         )
     def forward(self,x):
+        x = x.permute((0,2,1))
         return self.architecture(x)
