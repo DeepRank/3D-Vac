124ee92c2b70fe0a0cef7255121015e27d96f222
DanLep97 <danycmoilol@gmail.com>
2022-06-11 14:37:08 +0200

    trying new architecture with dropouts

--- src/5_train_models/CNN/models.py
+++ src/5_train_models/CNN/models.py
@@ -6,17 +6,19 @@ class MlpRegBaseline(nn.Module):
             nn.Flatten(),
 
             nn.Linear(input, neurons_per_layer),
-            nn.BatchNorm1d(neurons_per_layer),
+            # nn.BatchNorm1d(neurons_per_layer),
+            nn.Dropout(),
             nn.ReLU(),
 
             nn.Linear(neurons_per_layer, neurons_per_layer),
-            nn.BatchNorm1d(neurons_per_layer),
+            # nn.BatchNorm1d(int(neurons_per_layer/2)),
+            nn.Dropout(),
             nn.ReLU(),
 
             nn.Linear(neurons_per_layer, neurons_per_layer),
-            nn.BatchNorm1d(neurons_per_layer),
+            # nn.BatchNorm1d(int(neurons_per_layer/4)),
+            nn.Dropout(),
             nn.ReLU(),
-
 
             nn.Linear(neurons_per_layer, outputs),
         )
