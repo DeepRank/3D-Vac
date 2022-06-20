1d350ce398e080a30833f75f5a8d9d691ad1bbdc
DanLep97 <danycmoilol@gmail.com>
2022-06-20 15:31:30 +0200

    added batchnorm to mlp

--- src/5_train_models/CNN/models.py
+++ src/5_train_models/CNN/models.py
@@ -51,14 +51,17 @@ class MlpRegBaseline(nn.Module):
             # hidden layer
             nn.Linear(input, neurons_per_layer),
             nn.ReLU(),
-            nn.Dropout(), # half of weights are droped each forward()
+            # nn.Dropout(), # half of weights are droped each forward()
+            nn.BatchNorm1d(neurons_per_layer),
 
             nn.Linear(neurons_per_layer, neurons_per_layer),
             nn.ReLU(),
             nn.Dropout(), # half of weights are droped each forward()
+            nn.BatchNorm1d(neurons_per_layer),
 
             nn.Linear(neurons_per_layer, neurons_per_layer),
             nn.ReLU(),
+            nn.BatchNorm1d(neurons_per_layer),
 
             # output layer
             nn.Linear(neurons_per_layer, outputs),
