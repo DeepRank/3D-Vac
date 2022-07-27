bd9ab35bbde91aed451d7a3fcc555eba3810b2c7
Dario Marzella <dmarz@int3.local.snellius.surf.nl>
2022-07-27 13:22:09 +0200

    Remove zombie code, update help

--- src/0_build_db1/generate_db1_II.py
+++ src/0_build_db1/generate_db1_II.py
@@ -6,15 +6,10 @@ from math import exp, log
 
 # list of arguments:
 arg_parser = argparse.ArgumentParser(
-    description='Build a p:MHC Binding Affinity csv file for BA only with a precise value ("=" inequallity) from MHCFlurry dataset. For NetMHCpan dataset or MHCflurry EL, use another script. \n \
-    This script can be used to explore data by giving allele name and peptide length. Please provide the -d directive if you want to create a csv. This script is used to generate the DB1. \n \
+    description='Build a p:MHC Binding Affinity csv file for BA from NetMHCIIpan4.1 database. \n \
     The output csv file will have extended number of columns with the ID column (position 0) and the location of generated model in DB2 as the last column \n \
-    The database can be generated using https://github.com/openvax/mhcflurry/tree/master/downloads-generation/data_curated curate.py script. \
-    For a quick sight at the expected format: https://data.mendeley.com/datasets/zx3kjzc3yx/3 -> Data_S3.csv.\
     The output csv will have the same columns with the ID at the beggining and the path for the DB2 as the last one.\
-    Csv file columns: allele, peptide, ba_value, equality/inequality, measurement_type, measurement_kind etc..\
-    The measurement_type depends on the measurment_kind can be qualitative (elution, binding affinity) \
-    or quantitative (binding_affinity).\
+    Csv file columns: allele, peptide, score, measurement,etc..\
     ')
 arg_parser.add_argument(
     "--source-folder", "-f",
@@ -28,21 +23,6 @@ arg_parser.add_argument(
     default = None
 )
 
-# arg_parser.add_argument(
-#     "--peptide-length", "-P",
-#     help="Length of peptides.",
-#     #default=15,
-#     type=int
-# )
-# arg_parser.add_argument(
-#     "--allele", "-A",
-#     default=[],
-#     help="Name of the allele(s) to filter. \n \
-#         More than one allele have to be separated by spaces: -A HLA-A HLA-B. \n \
-#         The allele name can be resolved up the the last position (HLA-A*02:01) or only for a specie (HLA). \n \
-#         If no allele is provided, every alleles will be returned.",
-#     nargs="+",
-# )
 arg_parser.add_argument("--prefix", "-i",
     help="The prefix for the ID",
     choices=["EL", "BA"],
@@ -86,41 +66,6 @@ def adapt_allele_name(allele):
 
 if __name__ == "__main__":
     a = arg_parser.parse_args()
-    # build the authorized alleles list based on arguments provided
-    # rows = []
-
-    # input_csv_df = pd.read_csv(f"../../data/external/unprocessed/{a.source_csv}")
-    # ids = list(range(len(input_csv_df)))
-    # input_csv_df["ID"] = [f"{a.prefix}_{id+1}" for id in ids] # give an ID to each entry
-    # # PANDORA generated models location (provided as an argument for the modeling, among peptide and MHC allele):
-    # input_csv_df["db2_folder"] = [f"/projects/0/einf2380/data/pMHCI/models/{a.prefix}/{assign_outfolder(id+1)}" for id in ids]
-
-    # filter only discrete and quantitative measurements. This filter is applied for pilot study 
-    # as a pre-filder (before filtering alleles and peptide length):
-    # input_csv_df = input_csv_df.query("measurement_inequality == '=' & measurement_type == 'quantitative' & \
-    #     measurement_kind == 'affinity' & measurement_value >= 2")
-
-    # apply the allele and length of peptide filter:
-    # output_csv_df = input_csv_df
-    # if len(a.allele) > 0:
-    #     allele_mask = [ # boolean mask to filter the df for target alleles 
-    #         any(allele_filter in allele_id for allele_filter in a.allele) # return true if one of the a.allele string is in the allele_id string
-    #         for allele_id in input_csv_df["allele"].tolist() # convert the allele of the df into a list
-    #     ]
-    #     output_csv_df = input_csv_df[allele_mask] # filter for the rows which have the specified alleles
-
-    # if a.peptide_length > 0:
-    #     # simple mask to filter rows which have the desired peptide length:
-    #     peptide_mask = [len(peptide) == a.peptide_length for peptide in output_csv_df["peptide"].tolist()]
-    #     output_csv_df = output_csv_df[peptide_mask]
-
-    # #save the csv:
-    # if a.output_csv:
-    #     output_csv_df.to_csv(f"../../data/external/processed/{a.output_csv}", index=False)
-    #     print(f"file {a.output_csv} with {len(output_csv_df)} entries saved in data/external/processed/")
-    # else:
-    #     print(output_csv_df)
-
 ######################
     #%% 
     #Get a list of acceptable allele names (to differentate them from cell lines)
@@ -155,11 +100,6 @@ if __name__ == "__main__":
             ba_dict[x[0]][x[1]].append(x[2])
         except KeyError:
             ba_dict[x[0]] = {x[1]:[x[2]]}
-
-    # Pickle the data
-    # with open('../binding_data/pMHCII/ba_values.pkl', 'rb') as inpkl:
-    #     ba_values = pickle.load(inpkl)
-    #     ba_dict = pickle.load(inpkl)
 
     #Define output folder
     MHCII_outfolder = '/projects/0/einf2380/data/pMHCII/models/'
