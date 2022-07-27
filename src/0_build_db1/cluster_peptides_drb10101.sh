fe048e40228d7ec75149bc23a1af191230fbfa98
Dario Marzella <dmarz@int3.local.snellius.surf.nl>
2022-07-27 13:15:47 +0200

    update cluster_peptides_drb10101.sh to use the only-15mers file

--- src/0_build_db1/cluster_peptides_drb10101.sh
+++ src/0_build_db1/cluster_peptides_drb10101.sh
@@ -6,4 +6,4 @@
 #SBATCH --partition=thin
 #SBATCH --time=01:00:00
 
-python cluster_peptides.py -e -u -l 15 -n 64 -f /home/dmarz/3d-epipred/binding_data/pMHCII/IDs_BA_DRB0101_MHCII.csv
+python cluster_peptides.py -e -u -l 15 -n 64 -f ../../data/external/processed/IDs_BA_DRB0101_MHCII_15mers.csv
