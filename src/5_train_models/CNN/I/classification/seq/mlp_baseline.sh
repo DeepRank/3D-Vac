b09b1df26fb01ff3acf48da89243d2ff4d66bcea
DanLep97 <danycmoilol@gmail.com>
2022-09-06 19:32:31 +0200

    correcting seq branch

---
+++ src/5_train_models/CNN/I/classification/seq/mlp_baseline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition thin
+#SBATCH --nodes 1
+#SBATCH --ntasks 10
+#SBATCH --cpus-per-task 10
+#SBATCH --time 01:00:00
+#SBATCH -o /projects/0/einf2380/data/training_logs/mlp_classification_seq_cpu-%J.out
+
+## load modules
+source activate deeprank
+module load 2020
+module load OpenMPI/4.0.3-GCC-9.3.0
+# usage: srun python -u mlp_reg_baseline.py <arguments for the script>
+
+srun python -u mlp_baseline.py "$@"
