742bc81d3c2848a0665ae7bb7ec2633182144136
Heleen Severin <severin@int3.local.snellius.surf.nl>
2022-09-09 17:30:00 +0200

    put parameters on newline

--- src/0_build_db1/1_generate_db1_I.sh
+++ src/0_build_db1/1_generate_db1_I.sh
@@ -1,4 +1,6 @@
 python generate_db1_I.py \
     -f ../../data/external/unprocessed/pMHCI/curated_training_data.no_additional_ms.csv \
     -d ../../data/external/processed/I/BA_pMHCI.csv \
-    -P 9 -A HLA-A*02:01 -i BA
+    -P 9 \
+    -A HLA-A*02:01 \
+    -i BA
