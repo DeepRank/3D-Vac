eefc1ce76e1a817b9312897690b383c7cc37702c
Heleen Severin <severin@int3.local.snellius.surf.nl>
2022-09-09 16:21:28 +0200

    implements new PANDORA loading of database take batch size as an argument instead of recomputing it again

--- src/1_build_db2/modelling_job.py
+++ src/1_build_db2/modelling_job.py
@@ -30,14 +30,14 @@ arg_parser.add_argument("--num-cores", "-n",
     help="Number of cores to be used per node.",
     default=128
 )
-arg_parser.add_argument("--db-path", "-d",
-    help="Path to PANDORA database folder.",
-    default="/projects/0/einf2380/softwares/PANDORA/PANDORA_files/data/csv_pkl_files/20220708_complete_db.pkl"
+arg_parser.add_argument("--batch_size", "-b",
+    help="Batch size calculated by allocate_nodes.py.",
+    required=True
 )
 a = arg_parser.parse_args()
 
 print(f'INFO: \n cases per hour per node :{10*a.num_cores} \n num of cores: {a.num_cores}\n \
-running time:{int(a.running_time)}\nbatch: {10*a.num_cores*int(a.running_time)}')
+running time:{int(a.running_time)}\nbatch: {a.batch_size}')
 
 # determine node index so we don't do the same chunk multiple times
 node_index = int(os.getenv('SLURM_NODEID'))
@@ -49,32 +49,24 @@ if df.empty:
     sys.exit(0)
 
 
-cases_per_hour_per_node = 10*int(a.num_cores) # 10*128= 1280
-batch = cases_per_hour_per_node*int(a.running_time)
-
-start_row = (batch*node_index)
-end_row = (batch*node_index) + batch
+start_row = (a.batch_size*node_index)
+end_row = (a.batch_size*node_index) + a.batch_size
 # it is possible that the end row index exceeds the length of the file because the file length is not divisible by the batch size
 if end_row > df.shape[0]+1: 
     end_row = df.shape[0]+1 
 
-print(f'INFO: Node index: {node_index} \nStart row: {start_row} \nEnd row: {end_row} \nBatch size: {batch}')
+print(f'INFO: Node index: {node_index} \nStart row: {start_row} \nEnd row: {end_row} \nBatch size: {a.batch_size}')
 
 # Load the database file
 print('Loading Database..')
-db = Database.load(a.db_path)
+db = Database.load()
 print('Database loaded')
-#db.update_ref_sequences()
-PDB_path = a.db_path.split('/data/')[0] + '/data/PDBs'
-db.repath(PDB_path, save=False)
-print('Database repathed')            
-
 
 #find outdir column
 outdir_col = df.columns.to_list().index('db2_folder')
 
 # DEBUG
-print(f"INFO:\n path: {a.csv_path}\n db: {a.db_path}\n MHC_class={a.mhc_class}\n outdir_col={outdir_col} start_row={start_row}, end_row={end_row}\n num_cores={a.num_cores}")
+print(f"INFO:\n path: {a.csv_path}\n MHC_class={a.mhc_class}\n outdir_col={outdir_col} start_row={start_row}, end_row={end_row}\n num_cores={a.num_cores}")
 
 #Create targets
 t1 = time.time()
@@ -96,10 +88,3 @@ print(f"Time to model: {t3-t2}")
 
 wrapping_time = t2-t1
 modelling_time = t3-t2
-
-# wrapping_times = comm.gather(wrapping_time)
-# modelling_times = comm.gather(modelling_time)
-
-# if rank==0:
-#     print("Average time to create wrappers: ", float(np.array(wrapping_times).mean()))
-#     print("Average time to create models: ", float(np.array(modelling_times).mean()))
