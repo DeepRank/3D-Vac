fd070e8f9ce5c3b8580f6dd0c945191bba68cbcf
Heleen Severin <severin@int3.local.snellius.surf.nl>
2022-09-23 12:12:24 +0200

    remove copy_target_file function (this is handled in db3) change 'dir'variable names to prevent masking of builtin variable change parallel part to let one thread handle 1/n_cores*n_model_paths (not one thread per one model path like before)

--- src/1_build_db2/clean_outputs.py
+++ src/1_build_db2/clean_outputs.py
@@ -39,157 +39,131 @@ arg_parser.add_argument("--single-path", "-s",
     type=str,
     required=False)
 
+# check if case was modelled:
+def check_exist(case):
+    try:
+        # if both the folder and the tar exist, remove the folder and extract tar again (we expect the tar not to be touched)
+        if os.path.exists(f'{case}.tar') and os.path.exists(case):
+            subprocess.run(f'rm -r {case}', shell=True, check=True)
+            return case
+        elif os.path.exists(case):
+            print('Only the folder of case exists, removing folder')
+            subprocess.run(f'rm -r {case}', shell=True, check=True)
+        elif os.path.exists(f'{case}.tar'):
+            return case
+    except subprocess.CalledProcessError as e:
+        print('In check_exists: ')
+        print(e)
+
+def unpack_archive(case):
+    try:
+        with tarfile.open(f'{case}.tar', 'r') as tar:
+            # need to rename tar members because writing to the original dir will not work otherwise
+            for member in tar:
+                member.name = os.path.basename(member.name)
+            os.mkdir(case)
+            tar.extractall(case)
+    except tarfile.ReadError as e:
+        print(e)
+        # remove complete directory so that get_unmodelled cases sees it as unmodelled
+        print(f"(unpack archive) Tar file is not valid, removing: {case}")
+        subprocess.run(f'rm -r {case}.tar', shell=True, check=True)
+        return False
+    except subprocess.CalledProcessError as e:
+        print('In unpack archive: ')
+        print(e)
+    except:
+        traceback.print_exc()
+    assert os.path.exists(case) # if all goes well the tar file should exist
+    return True
+
+# cleaning of target directories, to be called in parallel
+def clean_target_dir(case):
+    for dir_wild in ['/*.DL*', '/*.B99990001.pdb', '/*.V99990001', '/*.D00000001', 
+                    '/__pycache__/', '/*.lrsr', '/*.rsr', '/*.sch']:
+        wild_paths = glob.glob(f'{case}{dir_wild}')
+        for file_path in wild_paths:
+            if os.path.exists(file_path):
+                try:
+                    if dir_wild[-1] == '/':
+                        subprocess.run(f'rm -r {file_path}', shell=True, check=True)
+                        #subprocess.check_call(f'rm -r {file_path}', shell=True)
+                    else:
+                        subprocess.run(f'rm {file_path}', shell=True, check=True)
+                except subprocess.CalledProcessError as e:
+                    print('In clean_target_dir: ')
+                    print(e)
+                except Exception as e:
+                    print(e)
+
+def zip_and_remove(case):
+    try:    
+        # remove the old archive, but only if already unzipped
+        if os.path.exists(f'{case}.tar') and os.path.exists(case):
+            subprocess.run(f"rm {case}.tar", shell=True, check=True)
+        # create new archive of the folder
+        with tarfile.open(f'{case}.tar', 'w') as archive: # create new tarfile to gather files in
+            case_files = glob.glob(os.path.join(case, '*'))
+            for case_file in case_files:
+                archive.add(case_file)
+        # check if tar was created correctly and remove the original files from the folder
+        if os.path.exists(f'{case}.tar'):
+            subprocess.run(f"rm -r {case}", shell=True, check=True)
+            return True
+        else:
+            print(f'Error creating archive: {case}.tar')
+    except subprocess.CalledProcessError as e:
+        print('In zip and remove: ')
+        print(e)
+
+def clean_copy_target_files(sub_folders):
+
+    for folder in sub_folders:
+        try:
+            case_dir = check_exist(folder)
+            # if case was modelled, do the next steps
+            if case_dir:
+                if not unpack_archive(folder): # skip case if files are not valid
+                    return 
+                clean_target_dir(folder)     
+                zip_and_remove(folder)
+        except:
+                print(f'Cleaning failed for case {folder}')
+                print(traceback.format_exc())
+                try:
+                    # try to archive folder and remove unarchived files after an exception has occured
+                    zip_and_remove(folder)
+                except:
+                    print(traceback.format_exc())
+
 a = arg_parser.parse_args()
 
 db2_selected_models_path = f"/projects/0/einf2380/data/pMHC{a.mhc_class}/db2_selected_models_1"
 
+# find the paths of the models inside the model_path folder
 if a.single_path:
-    folders = [a.single_path]
+    folders = [a.single_path.split('.')[0]]
     n_cores = 1
 else:
     # clean the whole models folder
     wildcard_path = os.path.join(a.models_path, '*/*')
     folders = glob.glob(wildcard_path)
-    folders = [dir.split('.')[0] for dir in folders]
+    folders = [folder for folder in folders if '.tar' in folder]
+    folders = [case.split('.')[0] for case in folders]
     n_cores = int(os.getenv('SLURM_CPUS_ON_NODE'))
 
-#######################################################
+# run the cleaning
+if not a.single_path:
+    # create list of lists (inner lists are list of paths), there are n_cores inner lists of approx equal length
+    all_paths_lists = []
+    chunk = math.ceil(len(folders)/n_cores)
+    # cut the process into pieces to prevent spawning too many parallel processing
+    for i in range(0, len(folders), chunk):
+        all_paths_lists.append(folders[i:min(i+chunk, len(folders))])
+    # let each inner list be handled by exactly one thread
+    Parallel(n_jobs = n_cores, verbose = 1)(delayed(clean_copy_target_files)(case) for case in all_paths_lists)
 
-# check if case was modelled:
-def check_exist(dir):
-    # if both the folder and the tar exist, remove the folder and extract tar again (we expect the tar not to be touched)
-    if os.path.exists(f'{dir}.tar') and os.path.exists(dir):
-        subprocess.Popen(f'rm -r {dir}', shell=True).wait()
-        return dir
-    elif os.path.exists(dir):
-        print('Only the folder of case exists, removing folder')
-        subprocess.Popen(f'rm -r {dir}', shell=True).wait()
-    elif os.path.exists(f'{dir}.tar'):
-        return dir
-    # elif os.path.exists(dir):
-    #     # arhive again if folder was unzipped during process, will return true if successful
-    #     return zip_and_remove(dir)
-        # print(f"Case: {dir} was not modelled")
-
-def unpack_archive(dir):
-    try:
-        with tarfile.open(f'{dir}.tar', 'r') as tar:
-            # need to rename tar members because writing to the original dir will not work otherwise
-            for member in tar:
-                member.name = os.path.basename(member.name)
-            os.mkdir(dir)
-            tar.extractall(dir)
-    except tarfile.ReadError as e:
-        print(e)
-        # remove complete directory so that get_unmodelled cases sees it as unmodelled
-        print(f"(unpack archive) Tar file is not valid, removing: {dir}")
-        subprocess.Popen(f'rm -r {dir}.tar', shell=True).wait()
-        return False
-    except:
-        traceback.print_exc()
-        return False
-    assert os.path.exists(dir) # if all goes well the tar file should exist
-    return True
-
-# cleaning of target directories, to be called in parallel
-def clean_target_dir(dir):
-    for dir_wild in ['/*.DL*', '/*.B99990001.pdb', '/*.V99990001', '/*.D00000001', 
-                    '/__pycache__/', '/*.lrsr', '/*.rsr', '/*.sch']:
-        wild_paths = glob.glob(f'{dir}{dir_wild}')
-        for file_path in wild_paths:
-            if os.path.exists(file_path):
-                if dir_wild[-1] == '/':
-                    subprocess.Popen(f'rm -r {file_path}', shell=True).wait()
-                else:
-                    subprocess.Popen(f'rm {file_path}', shell=True).wait()
-
-
-# zipping the folders to reduce the amount of files (inodes)
-def copy_target_files(case):
-    # get the best structure
-    molpdf_path = f"{case}/molpdf_DOPE.tsv"
-    if os.path.exists(molpdf_path):
-        molpdf_df = pd.read_csv(molpdf_path, sep="\t", header=None)
-        target_scores = molpdf_df.iloc[:,1].sort_values()[0:a.structure_rank]
-        target_mask = [score in target_scores.tolist() for score in molpdf_df.iloc[:,1]]
-        target_ids = molpdf_df[target_mask].iloc[:,0]
-        structures = [f"{case}/{structure}" for structure in target_ids]
-
-        # symlink each target
-        # attempt to create subfolders:
-        for structure in structures:
-            dir = "/".join(structure.split("/")[-4:-1])
-            pdb_file = structure.split("/")[-1].replace("_", "-")
-            destination_dir = f"{db2_selected_models_path}/{dir}/pdb"
-            destination_file = f"{destination_dir}/{pdb_file}"
-            try: # create remaining subfolders:
-                os.makedirs(destination_dir); 
-            except FileExistsError:
-                print('Destination file exists', destination_dir)
-                if os.path.exists(destination_file):
-                    print('best model alredy copied, next..')
-                    continue
-            except:
-                print('Something went wrong in creating ', destination_dir)
-                print(traceback.format_exc())
-            try: #make the symlink:
-                subprocess.check_call(f'cp {structure} {destination_file}', shell=True)
-            except:
-                print(traceback.format_exc())
-    # elif os.path.exists(f"{db2_selected_models_path}/pdb/*"): # this path might not be right need to change
-    #         print("Molpdf file was already moved: {case}")
-    else:
-        print(f'Molpdf path does not exist or was already moved: {case}')
-
-def zip_and_remove(case):    
-    # remove the old archive, but only if already unzipped
-    if os.path.exists(f'{case}.tar') and os.path.exists(case):
-        subprocess.Popen(f"rm {case}.tar", shell=True).wait()
-    # create new archive of the folder
-    with tarfile.open(f'{case}.tar', 'w') as archive: # create new tarfile to gather files in
-        case_files = glob.glob(os.path.join(case, '*'))
-        for case_file in case_files:
-            archive.add(case_file)
-    # check if tar was created correctly and remove the original files from the folder
-    if os.path.exists(f'{case}.tar'):
-        subprocess.Popen(f"rm -r {case}", shell=True).wait()
-        return True
-    else:
-        print(f'Error creating archive: {case}.tar, skipping the file removal')
-        return False
-
-def clean_copy_target_file(dir):
-
-    try:
-        case_dir = check_exist(dir)
-        # if case was modelled, do the next steps
-        if case_dir:
-            print('got through check')
-            if not unpack_archive(dir): # skip case if files are not valid
-                return 
-            clean_target_dir(dir)
-            copy_target_files(dir)       
-            zip_and_remove(dir)
-    except:
-        print(f'Cleaning failed for case {dir}')
-        print(traceback.format_exc())
-    try:
-        # try to archive folder and remove unarchived files after an exception has occured
-        zip_and_remove(dir)
-    except:
-        print(traceback.format_exc())
-
-step = math.ceil(len(folders)/10)
-
-# print(f'Cut folders: start:{start} end:{end}')
-# cut the process into pieces to make sure not too many files are unzipped at the same time
-for chunk in range(0,10):
-    start = chunk*step
-    end = start+step
-    if end > len(folders):
-        end = len(folders)
-    chunk_folders = folders[start:end]
-    print(f'Chunk folders: start:{start} end:{end}')
-    Parallel(n_jobs = n_cores, verbose = 1)(delayed(clean_copy_target_file)(case) for case in list(chunk_folders))
+else:
+    clean_copy_target_files(folders)
 
 print(f"Cleaning finished.")
