f4c300fe9c2bcc3592063872b8391507a1556122
Heleen Severin <severin@int3.local.snellius.surf.nl>
2022-10-04 15:02:50 +0200

    add nonabreviated 'input-csv' argument and add job suffix to logs

--- src/1_build_db2/1_build_db2_I.sh
+++ src/1_build_db2/1_build_db2_I.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name build_db2_I
-#SBATCH -o /projects/0/einf2380/data/modelling_logs/I/db2/build_db2_I.out
+#SBATCH -o /projects/0/einf2380/data/modelling_logs/I/db2/build_db2_I-%J.out
 #SBATCH -n 1
 #SBATCH -c 1
 
 source activate deeprank
 
-python -u build_db2.py -i /projects/0/einf2380/data/external/processed/I/BA_pMHCI.csv \
+python -u build_db2.py --input-csv /projects/0/einf2380/data/external/processed/I/BA_pMHCI.csv \
     --models-dir /projects/0/einf2380/data/pMHCI/models/BA_1/\*/\* \
     --mhc-class I \
     --num-nodes 10
--- src/1_build_db2/1_build_db2_II.sh
+++ src/1_build_db2/1_build_db2_II.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name build_db2_II
-#SBATCH -o /projects/0/einf2380/data/modelling_logs/I/db2/build_db2_II.out
+#SBATCH -o /projects/0/einf2380/data/modelling_logs/I/db2/build_db2_II-%J.out
 #SBATCH -n 1
 #SBATCH -c 1
 
 source activate deeprank
 
-python -u build_db2.py -i /projects/0/einf2380/data/external/processed/II/BA_pMHCII.csv \
+python -u build_db2.py --input-csv /projects/0/einf2380/data/external/processed/II/BA_pMHCII.csv \
     --models-dir \
     --mhc-class II \
     --num-nodes 10
