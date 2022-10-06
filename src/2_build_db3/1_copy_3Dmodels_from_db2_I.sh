f6388b26550fdac6247103683eacf55f900141d1
Heleen Severin <severin@int1.local.snellius.surf.nl>
2022-10-06 11:16:24 +0200

    change to cpus-per-task instead of number-of-tasks, add more comp time

--- src/2_build_db3/1_copy_3Dmodels_from_db2_I.sh
+++ src/2_build_db3/1_copy_3Dmodels_from_db2_I.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH -p thin
 #SBATCH -N 1
-#SBATCH -n 128 # number of tasks
-#SBATCH -c 1
-#SBATCH --time 01:00:00
-#SBATCH -o /projects/0/einf2380/data/modelling_logs/copy_3Dmodels_from_db2_I.out
+#SBATCH --cpus-per-task 128
+#SBATCH --time 01:30:00
+#SBATCH -o /projects/0/einf2380/data/modelling_logs/I/db3/copy_3Dmodels_from_db2_I-%J.out
 
 # Load modules:
 module load 2021
@@ -13,6 +12,6 @@ module load foss/2021a
 # Activate conda env:
 source activate deeprank
 
-srun python -u ./copy_3Dmodels_from_db2.py  --mhc-class I \
-     --csv-file /home/severin/3D-Vac/data/external/processed/I/BA_pMHCI.csv \
-     --models-path /projects/0/einf2380/data/pMHCI/3D_models/BA_1/\*/\*
+srun python ./copy_3Dmodels_from_db2.py  --mhc-class I \
+     --csv-file /projects/0/einf2380/data/external/processed/I/BA_pMHCI_all.csv \
+     --models-path /projects/0/einf2380/data/pMHCI/models/BA_1/\*/\*
