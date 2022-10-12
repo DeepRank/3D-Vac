585ea29191df5f8215dcedd00c49a648f4c2b775
Heleen Severin <severin@int1.local.snellius.surf.nl>
2022-10-12 10:19:33 +0200

    remove sbatch arguments to run as a normal bash script

--- src/1_build_db2/1_build_db2_I.sh
+++ src/1_build_db2/1_build_db2_I.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name build_db2_I
-#SBATCH -o /projects/0/einf2380/data/modelling_logs/I/db2/build_db2_I-%J.out
-#SBATCH -n 1
-#SBATCH -c 1
 
-source activate deeprank
+conda activate deeprank
 
-python -u build_db2.py --input-csv /projects/0/einf2380/data/external/processed/I/BA_pMHCI.csv \
-    --models-dir /projects/0/einf2380/data/pMHCI/models/BA_1/\*/\* \
+python -u build_db2.py --input-csv /home/severin/3D-Vac/data/external/processed/I/BA_pMHCI_mock499061.csv \
+    --models-dir /projects/0/einf2380/data/pMHCI/models/BA_4/\*/\* \
     --mhc-class I \
-    --num-nodes 10
+    --num-nodes 2
     
