a1467b4bece8c04bb3d6d1f23f5d8e435983f0cc
Heleen Severin <heleen.severin@radboudumc.nl>
2022-10-14 12:18:02 +0200

    change short arguments to long arguments and some other naming

--- src/0_build_db1/1_generate_db1_I_HLA-all.sh
+++ src/0_build_db1/1_generate_db1_I_HLA-all.sh
@@ -1,7 +1,9 @@
 python generate_db1_I.py \
-    -f ../../data/external/unprocessed/pMHCI/curated_training_data.no_additional_ms.csv \
-    -d ../../data/external/processed/I/BA_pMHCI.csv \
-    -P 0 \
-    -A HLA \
-    -i BA \
-    -e 
+    --source-csv /projects/0/einf2380/data/external/unprocessed/pMHCI/curated_training_data.no_additional_ms.csv \
+    --output-csv /projects/0/einf2380/data/external/processed/I/BA_pMHCI_all.csv \
+    --peptide-length 0 \
+    --allele HLA \
+    --prefix BA \
+    --measurement_type BA \
+    --include_inequality \
+    --include_qualitative
