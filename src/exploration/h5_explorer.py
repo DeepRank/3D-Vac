7e952206e0a6eac03de0fc9bdc2d199336540728
DanLep97 <danycmoilol@gmail.com>
2022-11-01 14:42:52 +0100

    last modifications before presentation

--- src/exploration/h5_explorer.py
+++ src/exploration/h5_explorer.py
@@ -18,6 +18,11 @@ arg_parser.add_argument("--name", "-n",
     help="Name of the subfolder in report/figures/h5explorer/ to save the plots.",
     default="test"
 )
+arg_parser.add_argument("--plot", "-P",
+    help="Provide this argument to save plots in the --name subfolder. By default it won't plot.",
+    default="False",
+    action="store_true"
+)
 
 a = arg_parser.parse_args()
 
@@ -35,24 +40,29 @@ for i,f in enumerate(h5_files):
                 k_mv_d = {k:[] for k in keys}
             for k in keys:
                 values = h5[f"{m}/mapped_features/Feature_ind/{k}/value"][()]
+                if "vdwaals" in k:
+                    max_values = values[values > 8000]
+                    if max_values.shape[0] >= 1:
+                        print(f"The model {m} has {max_values.shape[0]} values superior to 8000.")
                 if values.shape[0] == 0:
                     k_mv_d[k].append(0)
                 else:
                     k_mv_d[k].append(values.max())
 
-# make the dir if not already existing:
-plot_dir = f"../../reports/figures/h5explorer/{a.name}"
-if not os.path.isdir(plot_dir):
-    os.mkdir(plot_dir)
+if a.plot:
+    # make the dir if not already existing:
+    plot_dir = f"../../reports/figures/h5explorer/{a.name}"
+    if not os.path.isdir(plot_dir):
+        os.mkdir(plot_dir)
 
-# plot each feature:
-for feature in k_mv_d.keys():
-    values = np.array(k_mv_d[feature])
-    plt.hist(values, bins=100, log=True)
-    plt.title(f"Distribution of max {feature} values, mapped on the grid")
-    plt.xlabel("Max values for each model")
-    plt.ylabel("Log of bin count")
+    # plot each feature:
+    for feature in k_mv_d.keys():
+        values = np.array(k_mv_d[feature])
+        plt.hist(values, bins=100, log=True)
+        plt.title(f"Distribution of max {feature} values, mapped on the grid")
+        plt.xlabel("Max values for each model")
+        plt.ylabel("Log of bin count")
 
-    # save the plot:
-    plt.savefig(f"{plot_dir}/{feature}.png")
-    plt.close()
+        # save the plot:
+        plt.savefig(f"{plot_dir}/{feature}.png")
+        plt.close()
