0019c147449e3dfda34d9a672933ffddd72241fb
Heleen Severin <heleen.severin@radboudumc.nl>
2022-11-07 23:22:51 +0100

    improved readability of output and actually remove cases that are not valid

--- src/1_build_db2/get_unmodelled_cases.py
+++ src/1_build_db2/get_unmodelled_cases.py
@@ -48,22 +48,21 @@ arg_parser.add_argument("--n-structures", "-s",
     default=20,
 )
 
-def zip_and_remove(case):    
-    # remove the old archive, but only if already unzipped
-    if os.path.exists(f'{case}.tar') and os.path.exists(case):
-        subprocess.check_call(f"rm {case}.tar", shell=True)   
-    # create new archive of the folder
-    with tarfile.open(f'{case}.tar', 'w') as archive: # create new tarfile to gather files in
-        case_files = glob.glob(os.path.join(case, '*'))
-        for case_file in case_files:
-            archive.add(case_file)
-    # check if tar was created correctly and remove the original files from the folder
-    if os.path.exists(f'{case}.tar'):
-        subprocess.check_call(f"rm -r {case}", shell=True)
-        return True
-    else:
-        print(f'Error creating archive: {case}.tar, skipping the file removal')
-        return False
+def archive_and_remove(case):
+    """archives the case folder as a .tar file to save inode space
+
+    Args:
+        case str: directory name of case to be archived
+    """ 
+    prefix_case_folder = os.path.split(case.rstrip('/'))[0]
+    case_folder = os.path.split(case.rstrip('/'))[1]   
+    try:
+        subprocess.run(f"tar -cf {case}.tar -C {prefix_case_folder} {case_folder} \
+                       --remove-files", shell=True, check=True)
+    except subprocess.CalledProcessError as cpe:
+        print(f"Something went wrong in archive case: {case}\n{cpe}")
+    except Exception as e:
+        print(e)
 
 def get_archive_members(dir):
     try:
@@ -78,7 +77,7 @@ def get_archive_members(dir):
     except FileNotFoundError:
         if os.path.exists(dir):
             print(f"No tar found but directory exists: {dir}\n trying to archive again")
-            if zip_and_remove(dir):
+            if archive_and_remove(dir):
                 return get_archive_members(dir)
     except:
         print(traceback.print_exc())
@@ -105,16 +104,24 @@ def check_molpdf(case):
     except pd.errors.EmptyDataError:
         print(f'Molpdf empty: {case}')
 
+def remove_case(folder, reason):
+    try:
+        print(f'Removing case from models folder: {folder}, reason: {reason}')
+        subprocess.run(f'rm -r {folder}.tar', shell=True)
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
 
 def search_folders(folder):
     try:
         # Open output folder. If the folder doesn't have n_struc_per_case pdb files, it is considered as unmodelled.     #
         if a.archived:
             members = get_archive_members(folder)
-            if not members:
-                return
-            search_pdb = [re.search(r'BL.*\.pdb$', member) for member in members]
-            n_structures = sum((i is not None for i in search_pdb))
+            if members:
+                search_pdb = [re.search(r'BL.*\.pdb$', member) for member in members]
+                n_structures = sum((i is not None for i in search_pdb))
+            else:
+                remove_case(folder, "no files in tar")
         else:
             n_structures = len(glob.glob(f"{folder}/*.BL*.pdb"))
         if n_structures >= n_struc_per_case-1 and n_structures <= n_struc_per_case: # the n_structures <= n_struc_per_case is to be sure that no more than n_struc_per_case structures are
@@ -125,12 +132,8 @@ def search_folders(folder):
             if molpdf_present:
                 if check_molpdf(folder):
                     return case
-        try:
-            print(f'Removing case from models folder: {folder}')
-            subprocess.Popen(f'rm -r {folder}.tar', shell=True).wait()
-        except Exception as e:
-            print(e)
-            traceback.print_exc()
+        # remove case if the n_structures check failed
+        remove_case(folder, "not enough structures in folder")
     except:
         print(f'Failed to search folder for case {folder}')
         print(traceback.format_exc())
@@ -181,7 +184,7 @@ for bcase, casedir in zip(cases, model_dirs):
 if a.update_csv:
     df.to_csv(a.to_model, index=False) # the initial list of cases without the modelled cases
 else:
-    print('Unmodelled cases (excluding removed cases):')
+    print('Unmodelled cases (including removed cases):')
     not_modelled_ids = df['ID'].tolist()
     for mod_id in not_modelled_ids:
         print(mod_id)
