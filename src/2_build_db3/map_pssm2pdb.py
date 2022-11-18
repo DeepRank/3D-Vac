71310a52e9f0e42c713495fe0f3e79f6b435f83a
Heleen Severin <heleen.severin@radboudumc.nl>
2022-11-18 08:40:39 +0100

    use a general path for pssms instead of a single per allele and cross check with db2 to find right allele per case

--- src/2_build_db3/map_pssm2pdb.py
+++ src/2_build_db3/map_pssm2pdb.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import numpy as np
 from pssmgen import PSSM
 import os
+import traceback
 from mpi4py import MPI
 import subprocess
 
@@ -23,7 +24,7 @@ arg_parser.add_argument("--csv-file", "-i",
 )
 arg_parser.add_argument("--alphachain-pssm", "-M",
     help="""
-    MHC class alpha chain (M chain) raw pssm path
+    MHC class alpha chain (M chain) raw pssm path containing all mhc alleles
     """,
     required=True
 )
@@ -32,7 +33,6 @@ arg_parser.add_argument("--betachain-pssm", "-N",
     MHC class beta chain (N chain) raw pssm path
     """,
 )
-
 arg_parser.add_argument("--mhc-class", "-c",
     help="""
     MHC class
@@ -45,23 +45,34 @@ a = arg_parser.parse_args()
 
 csv_path = f"{a.csv_file}"
 df = pd.read_csv(csv_path)
+df['ID'] = df['ID'].apply(lambda x: '-'.join(x.split('_')))
 
 if rank == 0:
-    all_models = glob.glob(f"/projects/0/einf2380/data/pMHC{a.mhc_class}/db2_selected_models/BA/*/*/pdb/*.pdb")
-    db2 = np.array([model for model in all_models if Path(model).stem.split(".")[0] in df["ID"].tolist()])
-    db2 = np.array_split(db2, size)
+    all_models = []
+    all_models_first = glob.glob(f"/projects/0/einf2380/data/pMHC{a.mhc_class}/db2_selected_models_1/BA/*")
+    for folder in all_models_first:
+        all_models.extend(glob.glob(os.path.join(folder, '*/pdb/*.pdb')))
+    db2 = np.array_split(all_models, size)
 else:
     db2 = None
 
 db2 = comm.scatter(db2, root=0)
 
-if a.mhc_class == 'I':
-    chains = {'M':a.alphachain_pssm}
-elif a.mhc_class == 'II':
-    chains = {'M':a.alphachain_pssm, 'N':a.betachain_pssm}
-
 for case in db2:
     work_dir = "/".join(case.split("/")[:-2])
+    # check which allele belongs to case
+    allele = df[df['ID'] == Path(case).stem.split(".")[0]]['allele'].values[0]
+    try:
+        if a.mhc_class == 'I':
+            chain_M =  glob.glob(os.path.join(a.alphachain_pssm, f'pssm_raw/*{allele}*.pssm'))[0]
+            chains = {'M': chain_M}
+            
+        elif a.mhc_class == 'II':
+                chain_M = glob.glob(path.join(a.alphachain_pssm, f'pssm_raw/*dra*{allele}*.pssm'))[0]
+                chain_N = glob.glob(path.join(a.betachain_pssm, f'pssm_raw/*drb1*{allele}*.pssm'))[0]
+                chains = {'M': chain_M, 'N': chain_N}
+    except IndexError as ie:
+        print(f'Could not find allele of case {case} in pssm db\n{ie}\n{traceback.format_exc()}')
     try:
         subprocess.check_call(f'mkdir {work_dir}/pssm_raw', shell=True)
     except:
@@ -77,4 +88,4 @@ for case in db2:
     gen = PSSM(work_dir=work_dir)
     gen.map_pssm(pssm_dir="pssm_raw", pdb_dir="pdb", out_dir="pssm", chain=list(chains.keys()))
 
-print(f"Finished mapping {db2.shape[0]} PSSM to PDB on {rank}")
+print(f"Finished mapping {len(db2)} PSSM to PDB on {rank}")
