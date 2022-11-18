318f510f33282ce2a08b2339e9950d3d29d980e7
Heleen Severin <heleen.severin@radboudumc.nl>
2022-11-18 08:41:59 +0100

    take different template for alignment and add faster function for listing a large directory with pdbs

--- src/2_build_db3/5_align_pdb_I.sh
+++ src/2_build_db3/5_align_pdb_I.sh
@@ -4,7 +4,8 @@
 #SBATCH --ntasks-per-node=128
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=thin
-#SBATCH --time=01:00:00
+#SBATCH --time=03:00:00
+#SBATCH -o /projects/0/einf2380/data/modelling_logs/I/db3/align_pdb_I-%J.out
 
 # Load modules:
 module load 2021
@@ -13,9 +14,10 @@ module load foss/2021a
 # Activate conda env:
 source activate deeprank
 
-export pdbs_path='/projects/0/einf2380/data/pMHCI/db2_selected_models/BA/*/*/pdb/*.pdb'
-export original_pdb='/projects/0/einf2380/data/pMHCI/db2_selected_models/BA/55001_56000/BA_55224_3MRC/pdb/BA-55224.BL00160001.pdb'
-export alignment_template='/projects/0/einf2380/data/pMHCI/alingment/alignment_template.pdb'
+export pdbs_path='/projects/0/einf2380/data/pMHCI/db2_selected_models_1/BA_test/\*/\*/pdb/\*.pdb'
+export original_pdb='/projects/0/einf2380/data/pMHCI/db2_selected_models_1/BA/68001_69000/BA-68085/pdb/BA-68085.pdb'
+#export original_pdb='/projects/0/einf2380/data/pMHCI/db2_selected_models/BA/55001_56000/BA_55224_3MRC/pdb/BA-55224.BL00160001.pdb'
+export alignment_template='/projects/0/einf2380/data/pMHCI/alignment/alignment_template.pdb'
 
 #Copy the template file
 cp $original_pdb $alignment_template
