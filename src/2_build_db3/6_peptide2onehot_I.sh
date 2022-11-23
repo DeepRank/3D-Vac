dbd86aababc01853ef552b16a7f369db47664c97
Heleen Severin <heleen.severin@radboudumc.nl>
2022-11-23 15:47:03 +0100

    change ntaks-per-cpu to n-processes instead of cpus-per-task

--- src/2_build_db3/6_peptide2onehot_I.sh
+++ src/2_build_db3/6_peptide2onehot_I.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -p thin
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=128
-#SBATCH --time 06:00:00
+#SBATCH --ntasks-per-node=128
+#SBATCH --cpus-per-task=1
+#SBATCH --time 02:00:00
 #SBATCH -o /projects/0/einf2380/data/modelling_logs/I/db3/peptide2onehot_I-%J.out
 
 # Load modules:
--- src/2_build_db3/6_peptide2onehot_II.sh
+++ src/2_build_db3/6_peptide2onehot_II.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -p thin
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=64
+#SBATCH --ntasks-per-node=64
+#SBATCH --cpus-per-task=1
 #SBATCH --time 01:00:00
 #SBATCH -o /projects/0/einf2380/data/modelling_logs/peptide2onehot_II.out
 
