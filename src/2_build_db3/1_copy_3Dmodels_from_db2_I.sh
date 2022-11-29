68c61497fd3f5a5d120e1c8cc7045e9d81fae397
Heleen Severin <heleen.severin@radboudumc.nl>
2022-11-29 11:26:57 +0100

    change path of models

--- src/2_build_db3/1_copy_3Dmodels_from_db2_I.sh
+++ src/2_build_db3/1_copy_3Dmodels_from_db2_I.sh
@@ -2,7 +2,7 @@
 #SBATCH -p thin
 #SBATCH -N 1
 #SBATCH --cpus-per-task 128
-#SBATCH --time 01:30:00
+#SBATCH --time 02:30:00
 #SBATCH -o /projects/0/einf2380/data/modelling_logs/I/db3/copy_3Dmodels_from_db2_I-%J.out
 
 # Load modules:
@@ -14,5 +14,5 @@ source activate deeprank
 
 python copy_3Dmodels_from_db2.py  --mhc-class I \
      --csv-file /projects/0/einf2380/data/external/processed/I/BA_pMHCI_human_quantitative.csv \
-     --models-path /projects/0/einf2380/data/pMHCI/3d_models/BA_1/\*/\* \
+     --models-path /projects/0/einf2380/data/pMHCI/3d_models/BA/\*/\* \
      --best-models-path /projects/0/einf2380/data/pMHCI/db2_selected_models_1
