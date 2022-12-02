28d603ff441288397d0c8c648ba50a4a2ca7530d
Giulia Crocioni <ccrocion@int3.local.snellius.surf.nl>
2022-12-02 16:27:46 +0100

    improve data generation script and logging

--- src/3_build_db4/GNN/0_generate_features.py
+++ src/3_build_db4/GNN/0_generate_features.py
@@ -3,19 +3,27 @@ import glob
 import os
 import sys
 from deeprankcore.query import QueryCollection
+import logging
 
 ####### please modify here #######
-run_day = '23112022'
+run_day = '02122022'
 project_folder = '/home/ccrocion/snellius_data_sample/'
 csv_file_name = 'BA_pMHCI_human_quantitative.csv'
 models_folder_name = 'exp_nmers_all_HLA_quantitative'
 data = 'pMHCI'
 resolution = 'residue' # either 'residue' or 'atomic'
 interface_distance_cutoff = 15 # max distance in Å between two interacting residues/atoms of two proteins
-process_count = 8 # 32 # remember to set the same number in --cpus-per-task in 0_generate_hdf5.sh
+cpu_count = 8 # 32 # remember to set the same number in --cpus-per-task in 0_generate_hdf5.sh
 ##################################
 
-print('Script running has started ...')
+# Loggers
+_log = logging.getLogger('')
+_log.setLevel(logging.INFO)
+sh = logging.StreamHandler(sys.stdout)
+sh.setLevel(logging.INFO)
+_log.addHandler(sh)
+
+_log.info('Script running has started ...')
 
 if resolution == 'atomic':
 	from deeprankcore.query import ProteinProteinInterfaceAtomicQuery as PPIQuery
@@ -28,12 +36,15 @@ output_folder = f'{project_folder}data/pMHCI/features_output_folder/GNN/{resolut
 pdb_files = glob.glob(os.path.join(models_folder_path + '/pdb', '*.pdb'))
 csv_data = pd.read_csv(csv_file_path)
 csv_data.cluster = csv_data.cluster.fillna(-1)
-print(f'pdbs files paths loaded, {len(pdb_files)} pdbs found.')
 
-pssms_m = []
-pssms_p = []
-clusters = []
-bas = []
+if not os.path.exists(output_folder):
+	os.makedirs(output_folder)
+else:
+	sys.exit(f'{output_folder} already exists, please update output_folder name!')
+
+_log.info(f'pdbs files paths loaded, {len(pdb_files)} pdbs found.')
+
+queries = QueryCollection()
 
 for pdb_file in pdb_files:
 
@@ -42,51 +53,29 @@ for pdb_file in pdb_files:
     pssm_p = glob.glob(os.path.join(models_folder_path + '/pssm', pdb_id + '.P.*.pssm'))
     assert len(pssm_m) == 1
     assert len(pssm_p) == 1
-    pssms_m.append(pssm_m[0])
-    pssms_p.append(pssm_p[0])
-
     csv_id = pdb_id.replace('-', '_')
     assert csv_data[csv_data.ID == csv_id].shape[0] == 1
     cluster = csv_data[csv_data.ID == csv_id].cluster.values[0]
     ba = csv_data[csv_data.ID == csv_id].measurement_value.values[0]
-    clusters.append(cluster)
-    bas.append(ba)
 
-bas_bin = [int(float(ba) <= 500) for ba in bas]
+    queries.add(
+        PPIQuery(
+            pdb_path = pdb_file, 
+            chain_id1 = "M",
+            chain_id2 = "P",
+            distance_cutoff = interface_distance_cutoff,
+            targets = {
+                'binary': int(float(ba) <= 500), # binary target value
+                'BA': ba, # continuous target value
+                'cluster': cluster
+                },
+            pssm_paths = {
+                "M": pssm_m[0],
+                "P": pssm_p[0]
+                }))
 
-assert len(pssms_m) == len(pdb_files)
-assert len(pssms_p) == len(pdb_files)
-assert len(clusters) == len(pdb_files)
-assert len(bas) == len(pdb_files)
-assert len(bas_bin) == len(pdb_files)
+_log.info(f'Queries created and ready to be processed.\n')
 
-if not os.path.exists(output_folder):
-	os.makedirs(output_folder)
-else:
-	sys.exit(f'{output_folder} already exists, please update output_folder name!')
+output_paths = queries.process(f'{output_folder}/processed-data')
 
-queries = QueryCollection()
-
-for idx, pdb_file in enumerate(pdb_files):
-	queries.add(
-		PPIQuery(
-			pdb_path = pdb_file, 
-			chain_id1 = "M",
-			chain_id2 = "P",
-			distance_cutoff = interface_distance_cutoff,
-			targets = {
-				'binary': bas_bin[idx], # binary target value
-				'BA': bas[idx], # continuous target value
-				'cluster': clusters[idx]
-				},
-			pssm_paths = {
-				"M": pssms_m[idx],
-				"P": pssms_p[idx]
-				}))
-
-print(f'Queries created and ready to be processed.\n')
-
-# Note that preprocess() has also process_count parameter, that by default takes all available cpu cores.
-# BUT on Snellius the default will allocate 1 cpu core per task. Remember to set --cpus-per-task properly in the .sh script.
-output_paths = queries.process(f'{output_folder}/processed-data', process_count)
-print(f'Processing is done. hdf5 files generated are in {output_folder}.')
+_log.info(f'Processing is done. hdf5 files generated are in {output_folder}.')
--- src/3_build_db4/GNN/copy_snellius_data_sample.py
+++ src/3_build_db4/GNN/copy_snellius_data_sample.py
@@ -2,25 +2,35 @@
 import glob
 import shutil
 import os
+import logging
 
 
+####### please modify here #######
 src_dir = "/projects/0/einf2380/data/pMHCI/features_input_folder/exp_nmers_all_HLA_quantitative"
 dest_dir = "/home/ccrocion/snellius_data_sample/data/pMHCI/features_input_folder/exp_nmers_all_HLA_quantitative"
 n_files = 100
+##################################
+
+# Loggers
+_log = logging.getLogger('')
+_log.setLevel(logging.INFO)
+sh = logging.StreamHandler(sys.stdout)
+sh.setLevel(logging.INFO)
+_log.addHandler(sh)
 
 pdb_files = glob.glob(os.path.join(src_dir + '/pdb', '*.pdb'))
 count = 0
 for pdb_file in pdb_files:
     pdb_id = pdb_file.split('/')[-1].split('.')[0]
     pssm_files = glob.glob(os.path.join(src_dir + '/pssm', pdb_id + '.*.pssm'))
-    # not all peptides' pssms are present, selecting only the complete data points
+    # make sure to select complete data points
     if len(pssm_files) == 2:
         shutil.copy(pdb_file, dest_dir + '/pdb')
-        print(f'copied {pdb_file}')
+        _log.info(f'copied {pdb_file}')
         for pssm_file in pssm_files:
             shutil.copy(pssm_file, dest_dir + '/pssm')
-            print(f'copied {pssm_file}')
-        print('\n')
+            _log.info(f'copied {pssm_file}')
+        _log.info('\n')
         count += 1
         if count == n_files:
             break
