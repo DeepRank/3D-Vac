ae014f4fd3c83dac76ee71e1154d6f05739580b2
Heleen Severin <heleen.severin@radboudumc.nl>
2022-12-05 15:01:41 +0100

    change template pdb and change SBATCH argument to multiple cpus instead of multiple tasks

--- src/2_build_db3/5_align_pdb_I.sh
+++ src/2_build_db3/5_align_pdb_I.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
+#SBATCH --job-name align_pdbs
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=128
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=128
 #SBATCH --partition=thin
-#SBATCH --time=03:00:00
+#SBATCH --time=01:00:00
 #SBATCH -o /projects/0/einf2380/data/modelling_logs/I/db3/align_pdb_I-%J.out
 
 # Load modules:
@@ -14,9 +13,8 @@ module load foss/2021a
 # Activate conda env:
 source activate deeprank
 
-export pdbs_path='/projects/0/einf2380/data/pMHCI/db2_selected_models_1/BA_test/\*/\*/pdb/\*.pdb'
+export pdbs_path='/projects/0/einf2380/data/pMHCI/features_input_folder/exp_nmers_all_HLA_quantitative/pdb_renumbered/\*'
 export original_pdb='/projects/0/einf2380/data/pMHCI/db2_selected_models_1/BA/68001_69000/BA-68085/pdb/BA-68085.pdb'
-#export original_pdb='/projects/0/einf2380/data/pMHCI/db2_selected_models/BA/55001_56000/BA_55224_3MRC/pdb/BA-55224.BL00160001.pdb'
 export alignment_template='/projects/0/einf2380/data/pMHCI/alignment/alignment_template.pdb'
 
 #Copy the template file
@@ -25,11 +23,11 @@ cp $original_pdb $alignment_template
 srun --job-name first_align \
     python -u align_pdb.py --pdbs-path $pdbs_path \
     --template $alignment_template --n-cores 128
-# Orient the template file on the aligned peptides PCA
+Orient the template file on the aligned peptides PCA
 srun --job-name orient_peptides \
     --dependency=afterany:first_align \ 
     python -u orient_on_pept_PCA.py -pdbs_path $pdbs_path \ 
-    -template $alignment_template
+    --template $alignment_template
 # Align all the models to the re-oriented template file
 srun --dependency=afterany:first_align:orient_peptides \ 
     python -u align_pdb.py --pdbs-path $pdbs_path \ 
