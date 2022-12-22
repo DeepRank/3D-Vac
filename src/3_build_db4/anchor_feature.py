dcf9297c6f4096514a3a61cab7c6417d0d8d95e6
DanLep97 <danycmoilol@gmail.com>
2022-12-22 12:12:44 +0100

    added m_chain anchor counterpart

--- src/3_build_db4/anchor_feature.py
+++ src/3_build_db4/anchor_feature.py
@@ -1,14 +1,16 @@
 from pdb2sql import pdb2sql
+from pdb2sql import interface
 import pandas as pd
 from deeprank.features import FeatureClass
 from ast import literal_eval
 import glob
 import random
 import pickle
+import numpy as np
 
 class AnchorFeature(FeatureClass):
 
-    def __init__(self, pdbfile):
+    def __init__(self, pdbfile, contact_cutoff=5):
         super().__init__("Residue")
 
         one_letter = {
@@ -18,7 +20,9 @@ class AnchorFeature(FeatureClass):
             'GLY':'G', 'PRO':'P', 'CYS':'C'
         }
 
+        self.contact_cutoff = contact_cutoff
         self.db = pdb2sql(pdbfile)
+        self.idb = interface(self.db)
         self.df = pd.read_csv("/home/lepikhovd/3D-Vac/data/external/processed/all_hla.csv")
         self.peptide = [one_letter[res[1]] for res in self.db(chainID="P").get_residues()]
         anchors = self.df.loc[self.df["peptide"] == "".join(self.peptide)][["anchor_0", "anchor_1"]].values.tolist()[0]
@@ -29,18 +33,50 @@ class AnchorFeature(FeatureClass):
     def get_feature(self):
         # get the feature's human readable format as well as xyz coordinates:
         # extract the keys and xyz of each key:      
-        anch_keys = self.db.get("chainID, resSeq, resName", resSeq=self.anchors, chainID=["P"])
-        print(anch_keys)
-        anch_xyz = self.db.get("x,y,z", resSeq=self.anchors, chainID=["P"])
+        anch_p_keys = self.db.get("chainID, resSeq, resName", resSeq=self.anchors, chainID=["P"])
+        anch_p_xyz = np.array(self.db.get("x,y,z", resSeq=self.anchors, chainID=["P"]))
+
+        # those will be filled with m residues at the interface of anchors only:
+        anch_m_keys = []
+        anch_m_xyz = []
+
+        # get the xyz value of each atom in chain M
+        m_xyz = np.array(self.db.get("x,y,z", chainID=["M"]))
 
         # populate the dictionaries used to build the actual feature:
         hread, xyzval = {},{}
 
-        for key, xyz in zip(anch_keys, anch_xyz):
-            # print("anch_xyz:", anch_xyz)
+        # get all residues at the interface of both chains
+        itf_residue_pairs = self.idb.get_contact_residues(
+            cutoff=self.contact_cutoff,
+            return_contact_pairs = True,
+            chain1="P", 
+            chain2="M")
+
+        for (_, p_resSeq, p_resName), m_contact_residues in itf_residue_pairs.items():
+            # if the residue number is an anchor then add the M chain residues info to anchor_feature:
+            if str(p_resSeq) in self.anchors:
+                for _, m_resSeq, __ in m_contact_residues:
+                    anch_m_keys.extend(self.db.get("chainID, resSeq, resName", resSeq = m_resSeq, chainID = ["M"]))
+                    anch_m_xyz.extend(self.db.get("x,y,z", resSeq = m_resSeq, chainID = ["M"]))
+        
+        # for test purposes only:
+        # self.anch_m_keys = anch_m_keys
+        # self.anch_m_xyz = anch_m_xyz
+        # self.anch_p_keys = anch_p_keys
+        # self.anch_p_xyz = anch_p_xyz
+
+        # populate hread and xyzval with chain P:
+        for key, xyz in zip(anch_p_keys, anch_p_xyz):
             hread[tuple(key)] = [1.0]
             xyz_key = tuple([1] + xyz)
             xyzval[xyz_key] = [1.0]
+
+        # populate hread and xyzval with chain M:
+        for key, xyz in zip(anch_m_keys, anch_m_xyz):
+            hread[tuple(key)] = [0.0]
+            xyz_key = tuple([0] + xyz)
+            xyzval[xyz_key] = [0.0]
             
         # assigning these two variables allows to build the features
         self.feature_data["anch"] = hread
@@ -49,7 +85,6 @@ class AnchorFeature(FeatureClass):
 
 
 def __compute_feature__(pdb_data, featgrp, featgrp_raw, chain1, chain2):
-
     # instantiate and build the feature:
     anchfeat = AnchorFeature(pdb_data)
     anchfeat.get_feature();
@@ -61,8 +96,7 @@ def __compute_feature__(pdb_data, featgrp, featgrp_raw, chain1, chain2):
     # close:
     anchfeat.db._close()
 
-if __name__ == "__main__":
-    # pdb_file = "/projects/0/einf2380/data/pMHCI/features_input_folder/test_case/pdb/BA-87169.pdb"
+if __name__ == "__main__": #debuging purposes only
     df = pd.read_csv("../../data/external/processed/all_hla.csv")
     df["ID"] = df["ID"].apply(lambda x: x.replace("_","-"))
 
