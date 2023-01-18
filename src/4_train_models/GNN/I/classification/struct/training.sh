e6d59e740d466c3af0e08f4ef8be365557d39210
Giulia Crocioni <ccrocion@int2.local.snellius.surf.nl>
2023-01-18 16:53:40 +0100

    update training scripts

--- src/4_train_models/GNN/I/classification/struct/training.sh
+++ src/4_train_models/GNN/I/classification/struct/training.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name training_GNN
-# #SBATCH --partition gpu
-# #SBATCH --gpus 1
-#SBATCH --partition fat
+#SBATCH --partition gpu
+#SBATCH --gpus 1
+# #SBATCH --partition fat
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
-#SBATCH --cpus-per-task 16
-#SBATCH --time 1:00:00
+# #SBATCH --cpus-per-task 64
+#SBATCH --time 15:00:00
 #SBATCH -o /projects/0/einf2380/data/training_logs/I/training_GNN_residue_job-%J.out
 
 source activate deeprank_gpu
