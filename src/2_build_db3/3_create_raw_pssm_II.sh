17f1150304da90ca47230ef0664828268e5e9b53
DarioMarzella <dario.marzella@outlook.it>
2023-01-31 17:37:05 +0100

    Temporary commit

--- src/2_build_db3/3_create_raw_pssm_II.sh
+++ src/2_build_db3/3_create_raw_pssm_II.sh
@@ -3,5 +3,5 @@
 # Activate conda env:
 source activate deeprank
 
-python build_pssm_input.py --output-folder  --input-file-db1 
-python create_raw_pssm.py --workdir /projects/0/einf2380/data/pMHCII/pssm_raw/hla_drb1_0101
+python build_pssm_input.py --output-folder /projects/0/einf2380/data/pMHCII/pssm_raw/all_mhc --input-file-db1 /projects/0/einf2380/data/external/processed/II/IDs_BA_MHCII_noduplicates.csv
+python create_raw_pssm.py --workdir /projects/0/einf2380/data/pMHCII/pssm_raw/all_mhc --blast-db /projects/0/einf2380/data/blast_dbs/all_mhc/all_mhc
--- src/2_build_db3/4_map_pssm2pdb_II.sh
+++ src/2_build_db3/4_map_pssm2pdb_II.sh
@@ -18,10 +18,10 @@ source activate deeprank
 
 srun --job-name map_pssm \
     python -u map_pssm2pdb.py --mhc-class II \
-    --csv-file /projects/0/einf2380/data/external/processed/II/IDs_BA_DRB10101_MHCII_15mers.csv \
-    --alphachain-pssm /projects/0/einf2380/data/pMHCII/pssm_raw/hla_drb1_0101/pssm_raw/hla_dra_0101.pssm \
-    --betachain-pssm /projects/0/einf2380/data/pMHCII/pssm_raw/hla_drb1_0101/pssm_raw/hla_drb1_0101.pssm
+    --csv-file /projects/0/einf2380/data/external/processed/II/IDs_BA_MHCII_noduplicates.csv \
+    --alphachain-pssm /projects/0/einf2380/data/pMHCII/pssm_raw/all_mhc \
+    --betachain-pssm /projects/0/einf2380/data/pMHCII/pssm_raw/all_mhc
 
-srun --dependency=afterany:map_pssm \
-    python merge_pdbs_and_pssms_chains.py \
-    --input-folders /projects/0/einf2380/data/pMHCII/db2_selected_models/BA/\*/\*
+#srun --dependency=afterany:map_pssm \
+#    python merge_pdbs_and_pssms_chains.py \
+#    --input-folders /projects/0/einf2380/data/pMHCII/db2_selected_models/BA/\*/\*
