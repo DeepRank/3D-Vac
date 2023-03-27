818b9d57480c8fc4e6e551a81e12bb94725f3f9a
Giulia Crocioni <ccrocion@int4.local.snellius.surf.nl>
2023-03-27 10:42:24 +0200

    add alleles in add_targets script for deeprankcore

--- src/3_build_db4/GNN/add_targets.py
+++ src/3_build_db4/GNN/add_targets.py
@@ -9,7 +9,7 @@ from collections import Counter
 import logging
 import sys
 
-############# Modify
+############ Modify
 run_day_data = '230202' # 100k data points (proj folder)
 # run_day_data = '230130' # 692 data points (local folder)
 protein_class = 'I'
@@ -17,16 +17,20 @@ target_data = 'BA'
 resolution_data = 'residue' # either 'residue' or 'atomic'
 project_folder = '/projects/0/einf2380'
 # project_folder = '/home/ccrocion/snellius_data_sample' # local resized df path
-hdf5_group = 'target_values/cluster'
+hdf5_target_group = 'target_values'
+hdf5_target_dataset = 'allele_type' # 'cluster'
+dataset_type = 'string'
 csv_file_path = '/projects/0/einf2380/data/external/processed/I/clusters/BA_pMHCI_human_quantitative_all_hla_gibbs_clusters.csv'
-cluster_col = 'cluster_set_10'
+csv_target_col = 'allele_type' #'cluster_set_10'
 #############
+hdf5_target_path = hdf5_target_group + '/' + hdf5_target_dataset
 folder_data = f'{project_folder}/data/pMHC{protein_class}/features_output_folder/GNN/{resolution_data}/{run_day_data}'
 input_data_path = glob.glob(os.path.join(folder_data, '*.hdf5'))
 output_folder = f'{project_folder}/data/pMHC{protein_class}/features_output_folder/GNN/{resolution_data}/{run_day_data}'
 csv_data = pd.read_csv(csv_file_path)
-csv_data['allele_type'] = csv_data.allele.str.extract(r'HLA-(\w)\*.+')
-log_file_name = hdf5_group.split('/')[1]
+if hdf5_target_dataset == 'allele_type':
+    csv_data[hdf5_target_dataset] = csv_data.allele.str.extract(r'HLA-(\w)\*.+')
+log_file_name = hdf5_target_path.split('/')[1]
 
 
 def add_targets():
@@ -53,35 +57,43 @@ def add_targets():
                 for mol in hdf5.keys():
                     p = re.compile('residue-ppi-(\w+-\d+):M-P')
                     csv_id = p.findall(mol)[0]
-                    cluster_value = csv_data.loc[csv_data['ID'] == csv_id][cluster_col].values[0]
-                    hdf5[mol][hdf5_group][()] = cluster_value
+                    target_value = csv_data.loc[csv_data['ID'] == csv_id][csv_target_col].values[0]
+                    if hdf5_target_path in hdf5[mol]:
+                        hdf5[mol][hdf5_target_path][()] = target_value
+                    else:
+                        hdf5[mol][hdf5_target_group].create_dataset(name=hdf5_target_dataset, data=target_value)
 
         except Exception as e:
             _log.error(e)
             _log.info(f'Error in opening {fname}, please check the file.')
 
     # verify
-    csv_cluster = []
-    hdf5_cluster = []
+    csv_target = []
+    hdf5_target = []
     count = 0
     for fname in input_data_path:
         with h5py.File(fname, 'r') as hdf5:
             for mol in hdf5.keys():
                 p = re.compile('residue-ppi-(\w+-\d+):M-P')
                 csv_id = p.findall(mol)[0]
-                cluster_value = csv_data.loc[csv_data['ID'] == csv_id][cluster_col].values[0]
-                if not cluster_value == hdf5[mol][hdf5_group][()]:
+                target_csv_value = csv_data.loc[csv_data['ID'] == csv_id][csv_target_col].values[0]
+                if dataset_type == 'string':
+                    target_hdf5_value = hdf5[mol][hdf5_target_path].asstr()[()]
+                else:
+                    target_hdf5_value = hdf5[mol][hdf5_target_path][()]
+                if not target_csv_value == target_hdf5_value:
                     _log.warning(f'\nSomething went wrong for data point with id {mol} in {fname}.')
-                    _log.info(f'HDF5 value: {hdf5[mol][hdf5_group][()]}')
-                    _log.info(f'csv cluster value {cluster_value}')
-                csv_cluster.append(cluster_value)
-                hdf5_cluster.append(hdf5[mol][hdf5_group][()])
+                    _log.info(f'HDF5 value: {hdf5[mol][hdf5_target_path][()]}')
+                    _log.info(f'CSV cluster value {target_value}')
+                csv_target.append(target_csv_value)
+                hdf5_target.append(target_hdf5_value)
                 count +=1
                 if count % 10000 == 0:
                     _log.info(f'{count} data points modified.')
-    _log.info(f'\n{cluster_col} values in the csv corresponding to data in the HDF5 files: {Counter(csv_cluster)}\n')
-    _log.info(f'Number of data points in the HDF5 files: {len(hdf5_cluster)}\n')
-    _log.info(f'{cluster_col} values in the HDF5 files: {Counter(hdf5_cluster)}')
+    _log.info(f'Number of data points in the CSV file used in the HDF5 files: {len(csv_target)}')
+    _log.info(f'{csv_target_col} values in the CSV corresponding to data in the HDF5 files:\n{Counter(csv_target)}\n')
+    _log.info(f'Number of data points in the HDF5 files: {len(hdf5_target)}')
+    _log.info(f'{hdf5_target_dataset} values in the HDF5 files:\n{Counter(hdf5_target)}')
 
 if __name__ == "__main__":
 	add_targets()
