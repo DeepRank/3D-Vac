0674f7a4ff0f2862aafd93d677b25d9037ac4991
Giulia Crocioni <ccrocion@int6.local.snellius.surf.nl>
2023-10-06 09:22:28 +0200

    update deeprankcore to deeprank2 in 4_train_models/DeepRank2/CNN/

---
+++ src/4_train_models/DeepRank2/CNN/training.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name deeprank2_training_CNN
+#SBATCH --partition gpu
+#SBATCH --gpus 1
+# #SBATCH --partition fat
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 16
+#SBATCH --time 20:00:00
+#SBATCH -o /projects/0/einf2380/data/logs/deeprank2/deeprank2_training_CNN-%J.out
+
+source activate deeprank2_gpu
+python -u training.py
