bdf8fe335208fa0018c781e7d2dd3a3e972006e5
DarioMarzella <dario.marzella@outlook.it>
2024-02-12 10:40:21 +0100

    Update scripts for manuscript plotting. Fix path issue in cnn_onefold.py

--- src/5_train_models/DeepRank/I/cnn_onefold.py
+++ src/5_train_models/DeepRank/I/cnn_onefold.py
@@ -2,8 +2,8 @@ import argparse
 import os.path as path
 import os
 import sys
-sys.path.append(path.abspath("../../../"))
-from DeepRank.I.classification.seq import data_path # path to the data folder relative to the location of the __init__.py file
+sys.path.append(path.abspath("../../"))
+#from DeepRank.I.classification.seq import data_path # path to the data folder relative to the location of the __init__.py file
 from DeepRank.CNN_models import *
 from DeepRank.NeuralNet import NeuralNet
 # import multiprocessing as mp
